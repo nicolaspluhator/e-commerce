283272761036317c529b0b40f2386aeb29058d2a
Cristian Nicolas <nicolaspluhator@gmail.com>
2023-09-29 10:16:05 -0300

    Se realizaron cambios en la estructura y new proy.

---
+++ docs/roadmap.md
@@ -0,0 +1,3 @@
+# Roadmap
+
+## Iteración 1
---
+++ ecommerce/src/main/java/edu/unam/ecommerce/controladores/ProductoController.java
@@ -0,0 +1,42 @@
+package edu.unam.ecommerce.controladores;
+
+import edu.unam.ecommerce.modelo.Producto;
+import edu.unam.ecommerce.servicios.ProductoServicio;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@Slf4j
+public class ProductoController {
+    
+    @Autowired
+    private ProductoServicio productoServicio;
+    
+    public ProductoController(ProductoServicio productoServicio){
+        this.productoServicio = productoServicio;
+    }
+    
+    @GetMapping("/productos")
+    public String index(Model model){
+        var productos = productoServicio.buscarProductos();
+        model.addAttribute("productos", productos);
+        return "producto/index";
+    }
+    
+    @GetMapping("/productos/crear")
+    public String create(Model model){
+        var producto = new Producto();
+        model.addAttribute("producto", producto);
+        return "producto/create";
+    }
+    
+    @PostMapping("/productos")
+    public String store(Producto producto){
+        productoServicio.agregarProducto(producto);
+        return "redirect:/productos";
+    }
+}
